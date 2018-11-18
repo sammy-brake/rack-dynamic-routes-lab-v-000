48697b6c3af909c9b7a1d27f6540a59c462a6d17
Samantha brake <sammy.brake@gmail.com>
2018-11-18 14:31:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,18 +7,17 @@ class Application
     req = Rack::Request.new(env)
     item = req.path.split("/").pop
      if req.path.match(/items/)
-     if @@items.include?(item)
-        resp.write item.price
+       if @@items.include?(item)
+         resp.write item.price
     #     binding.pry
+        else
+          resp.write "Item not found"
+          resp.status = 400
+        end
       else
-        resp.write "Item not found"
-        resp.status = 400
-      end
-    else
       resp.write "Route not found"
       resp.status = 404
-    end
+      end
     resp.finish
   end
-
 end
