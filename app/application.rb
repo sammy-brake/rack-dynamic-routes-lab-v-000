37bfad912de84630e645cfa31c9503d9d400560b
Samantha brake <sammy.brake@gmail.com>
2018-11-18 14:15:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    if req.path.match(/items/<itemname>)
+    if req.path.match(/items/itemname)
       binding.pry
     else
       resp.write "Route not found"
