8c74f99266c838d0d1ddfd5086980ba1bd06cde0
Samantha brake <sammy.brake@gmail.com>
2018-11-18 14:12:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    if req.path.match(/items/)
+    else 
+      resp.write "Route not found"
+      resp.status = 404
     resp.finish
   end
 
