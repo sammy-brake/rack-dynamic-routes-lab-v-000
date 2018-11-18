766131ba92dad987a045e2b7b544c6129057005d
Samantha brake <sammy.brake@gmail.com>
2018-11-18 14:30:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     item = req.path.split("/").pop
-
-    # if req.path.match(/items/)
+     if req.path.match(/items/)
      if @@items.include?(item)
         resp.write item.price
     #     binding.pry
