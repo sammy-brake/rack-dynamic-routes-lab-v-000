0ab9877ec383aea04a17b3d21335566827fa7bec
Samantha brake <sammy.brake@gmail.com>
2018-11-18 14:41:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,6 +15,7 @@ class Application
           resp.write "Item not found"
           resp.status = 400
       end
+    end 
       # else
       #   resp.write "Item not found"
       #   resp.status = 400
