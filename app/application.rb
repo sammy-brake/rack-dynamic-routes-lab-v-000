6d298e2f1f15412dfb39a412d147a7d93ad5a155
Samantha brake <sammy.brake@gmail.com>
2018-11-18 14:51:43 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    item = req.path.split("/").pop
+
+    if req.path.match(/items/)
+      # binding.pry
+      list = []
+      @@items.map do |product|
+        if product.name == item
+          list << product
+        end
+      end
+      if @@items.include?(list[0])
+        resp.write list[0].price
+      else
+        resp.write "Item not found"
+             resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+    resp.finish
+  end
+end
