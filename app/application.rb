9fe8e4175bec038520b3c877e5b6b4e0148009eb
Samantha brake <sammy.brake@gmail.com>
2018-11-18 14:39:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,11 +7,13 @@ class Application
     req = Rack::Request.new(env)
     item = req.path.split("/").pop
     if req.path.match(/items/)
-      #if the item is in @@items, print the price 
-      else 
-        resp.write "Item not found"
-        resp.status = 400
-      end
+      #if the item is in @@items, print the price
+      @@items.each do |individual_item|
+      end 
+      # else
+      #   resp.write "Item not found"
+      #   resp.status = 400
+      # end
     else
       resp.write "Route not found"
       resp.status = 404
